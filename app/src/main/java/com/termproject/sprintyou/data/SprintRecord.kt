64cc647c0cadf4a1022f550fd9493357b9259aed
squareCaaat <danny79052@gmail.com>
2025-11-20 00:37:16 +0900

    feat(data): 목표 Record 정의

---
+++ app/src/main/java/com/termproject/sprintyou/data/SprintRecord.kt
@@ -0,0 +1,22 @@
+package com.termproject.sprintyou.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "sprint_records")
+data class SprintRecord(
+    @PrimaryKey(autoGenerate = true)
+    val uid: Int = 0,
+    @ColumnInfo(name = "goal_content")
+    val goalContent: String,
+    @ColumnInfo(name = "target_duration")
+    val targetDurationSeconds: Long,
+    @ColumnInfo(name = "actual_duration")
+    val actualDurationSeconds: Long,
+    @ColumnInfo(name = "created_at")
+    val createdAt: Long
+)
+
+
+
